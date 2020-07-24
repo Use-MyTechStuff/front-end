dd14e3cde087c3d94346d1ffae28239b3094fd2f
Angela Plank <angelaplanknm@gmail.com>
2020-07-23 18:02:14 -0600

    basic signup and signin forms created

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Signup from "./Components/Signup"
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>New User Form</h1>
+      <Signup />
+      
+    </div>
+  );
+}
